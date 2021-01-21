b1e067cb98a78f41f5e79c72422f76ade1b775dd
Agustin Tiseira <tiseiraagustin50@gmail.com>
2021-01-21 02:06:01 -0300

    levantar server

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const server = require('./src/app.js');
+const { conn } = require('./src/db');
 
-
-const app = express();
-
-require('./db')
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded ({ extended: true}));
-
-app.get('/', (req,res)=> {
-  res.send('Hola')
-})
-
-app.listen(3001, ()=> {
-  console.log('Server up')
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
 });
